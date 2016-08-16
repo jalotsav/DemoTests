539d77a33468016a881578370a376ffcc92b0371
Jaldeep Asodariya <jdpatel.android@gmail.com>
2016-08-16 15:09:22 +0530

    HelloWorld text update on Runtime.

--- app/src/main/java/com/jalotsav/demotests/MainActivity.java
+++ app/src/main/java/com/jalotsav/demotests/MainActivity.java
@@ -1,7 +1,8 @@
 package com.jalotsav.demotests;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvHW = (TextView) findViewById(R.id.tv_mainactvty);
+        tvHW.setText("Jaldeep Asodariya");
     }
 }
