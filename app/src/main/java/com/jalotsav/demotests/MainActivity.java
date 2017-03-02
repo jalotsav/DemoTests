ff2fe7d105e598a1cd325cc0eee34d3bdbd7e097
Jaldeep Asodariya <jdpatel.android@gmail.com>
2017-03-02 16:20:56 +0530

    - BtnBranch add commit, Show Toast on click of Button

--- app/src/main/java/com/jalotsav/demotests/MainActivity.java
+++ app/src/main/java/com/jalotsav/demotests/MainActivity.java
@@ -2,7 +2,10 @@ package com.jalotsav.demotests;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tvHW = (TextView) findViewById(R.id.tv_mainactvty);
         tvHW.setText("Jaldeep Asodariya, Android Developer");
+
+        findViewById(R.id.btnTest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Test button click.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
